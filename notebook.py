d76ddd8fc299931356be8eb7e42ef8c9cd5ac451
Naturarerum <naturarerum@yahoo.fr>
2019-06-05 23:24:07 -0400

    version 05 juin

--- notebook.py
+++ notebook.py
@@ -1,4 +1,11 @@
 import datetime
 
 class Note:
-  """represents a note in the norebook"""
+  """represents a note in the notebook"""
+  def __init__(self, memo):
+    self.memo = memo
+
+
+  def create_note(self):
+    date_creation = datetime
+    return date_creation
