9d45c2242f569147bc0ce286b7ae1bb286b1d762
Naturarerum <naturarerum@yahoo.fr>
2019-06-05 23:43:19 -0400

    tags added

--- notebook.py
+++ notebook.py
@@ -2,8 +2,9 @@ import datetime
 
 class Note:
   """represents a note in the notebook"""
-  def __init__(self, memo):
+  def __init__(self, memo, tag):
     self.memo = memo
+    self.tag = tag
 
 
   def create_note(self):
